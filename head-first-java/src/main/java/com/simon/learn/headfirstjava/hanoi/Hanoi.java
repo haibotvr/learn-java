a2f860621efe6addab1edf1616087b9b224b9187
weihaibo <weihaibo@bogon>
2018-11-26 15:19:00 +0800

    hanoi

---
+++ head-first-java/src/main/java/com/simon/learn/headfirstjava/hanoi/Hanoi.java
@@ -0,0 +1,35 @@
+package com.simon.learn.headfirstjava.hanoi;
+
+public class Hanoi {
+
+    private int number = 0;
+
+    /**
+     *
+     * @param n 盘子的数目
+     * @param origin 源座
+     * @param assist 辅助座
+     * @param destination 目的座
+     */
+    public void hanoi(int n, char origin, char assist, char destination){
+        if(n == 1){
+            move(n, origin, destination);
+        }else{
+            hanoi(n - 1, origin, destination, assist);
+            move(n - 1, origin, destination);
+            hanoi(n - 1, assist, origin, destination);
+        }
+    }
+
+    private void move(int n, char origin, char destination){
+        number ++;
+        System.out.println("Direction:" + number + " from " + origin + " to " + destination);
+    }
+
+    public static void main(String[] args) {
+        Hanoi hanoi = new Hanoi();
+        hanoi.hanoi(1, 'A', 'B', 'C');
+    }
+
+
+}
