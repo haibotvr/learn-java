b82270b405dbfebd5e7d663d6ca22b64e64c74c9
weihaibo <weihaibo@bogon>
2020-08-24 15:52:19 +0800

    quick sort

--- algorithm/src/com/simon/learn/algorithm/sort/MergeSort.java
+++ algorithm/src/com/simon/learn/algorithm/sort/MergeSort.java
@@ -10,7 +10,52 @@ public class MergeSort {
      * 如果要排序一个数组，我们先把数组从中间分成前后两部分，然后对前后两部分分别排序，再将排好序的两部分合并在一起，这样整个数组就都有序了。
      * @param arr
      */
-    public static void mergeSort(int[] arr) {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if(left < right) {
+            int mid = left + (right - left) / 2;
+            //对左边数组排序
+            mergeSort(arr, left, mid);
+            //对右边数组排序
+            mergeSort(arr, mid + 1, right);
+            //合并2边数组
+            merge(arr, left, mid, right);
+        }
 
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        //定义临时数组大小
+        int[] temp = new int[right - left + 1];
+        //新数组索引
+        int i = 0;
+        //需要合并的2个数组起点
+        int j = left;
+        int k = mid + 1;
+        //把较小的数先移动到新的数组中
+        while(j <= mid && k <= right) {
+            if(arr[j] < arr[k]) {
+                temp[i++] = arr[j++];
+            }else {
+                temp[i++] = arr[k++];
+            }
+        }
+        //把左边剩余的数移入数组
+        while(j <= mid) {
+            temp[i++] = arr[j++];
+        }
+        //把右边剩余的数移入数组
+        while(k <= right) {
+            temp[i++] = arr[k++];
+        }
+        //将临时数组的数据覆盖到arr
+        for(int l = 0; l < temp.length; l++) {
+            arr[l + left] = temp[l];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 2, 9, 8, 1, 6, 3, 5, 7};
+        mergeSort(array, 0, array.length - 1);
+        ArrayUtil.log(array);
     }
 }
--- algorithm/src/com/simon/learn/algorithm/sort/QuickSort.java
+++ algorithm/src/com/simon/learn/algorithm/sort/QuickSort.java
@@ -5,24 +5,60 @@ package com.simon.learn.algorithm.sort;
  */
 public class QuickSort {
 
-    static void quickSort(int[] arr, int left, int right){
-        int f, t;
-        int ltemp, rtemp;
-        ltemp = left;
-        rtemp = right;
-        f = arr[(left + right) / 2];
-        while (ltemp < right){
-            while (arr[ltemp] < f){
-                ++ltemp;
+    //数组下标i, j的元素互相交换位置
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static int partitionIt(int[] array, int left, int right) {
+        //为什么 j加一个1，而i没有加1,是因为下面的循环判断是从--j和++i开始的.
+        //而基准元素选的array[left],即第一个元素，所以左游标从第二个元素开始比较
+        int i = left;
+        int j = right + 1;
+        //pivot 为选取的基准元素（头元素）
+        int pivot = array[left];
+        while (true) {
+            while(i<right && array[++i] < pivot){}
+            while(j > 0 && array[--j] > pivot){}
+            //左右游标相遇时候停止， 所以跳出外部while循环
+            if(i >= j) {
+                break;
+            }else {
+                //左右游标未相遇时停止, 交换各自所指元素，循环继续
+                swap(array, i, j);
             }
-            while (arr[rtemp] > f){
-                --rtemp;
-            }
-            if(ltemp <= rtemp){
-                t = arr[ltemp];
-                arr[ltemp] = arr[rtemp];
-            }
+
         }
+        //基准元素和游标相遇时所指元素交换，为最后一次交换
+        swap(array, left, j);
+        //一趟排序完成，返回基准元素位置(注意这里基准元素已经交换位置了)
+        return j;
+    }
+
+    private static void recQuickSort(int[] array, int left, int right){
+        if(right <= left){
+            //终止递归
+            return;
+        }else{
+
+            int partition = partitionIt(array, left, right);
+            //对上一轮排序(切分)时，基准元素左边的子数组进行递归
+            recQuickSort(array, left, partition - 1);
+            //对上一轮排序(切分)时，基准元素右边的子数组进行递归
+            recQuickSort(array, partition + 1, right);
+        }
+    }
+
+    public static void sort(int[] array){
+        recQuickSort(array, 0, array.length - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {9, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sort(array);
+        ArrayUtil.log(array);
     }
 
 }
